9a5542618e0ba05bb72b7e6de93e36daeb65cdeb
bigfish3012 <lichengkun0907@gmail.com>
2025-11-09 00:55:21 -0800

    set mappa

---
+++ final_project/readme.md
@@ -0,0 +1,3 @@
+mappa - https://github.com/cvalenzuela/Mappa
+
+set up mappa - https://editor.p5js.org/hirofumimatsuzaki/sketches/ByHZs4ryV
--- final_project/sketch.js
+++ final_project/sketch.js
@@ -1,4 +1,28 @@
-function setup () {
-  createCanvas(600, 400);
-  background(220);
+// set up source from: https://editor.p5js.org/hirofumimatsuzaki/sketches/ByHZs4ryV
+const mappa = new Mappa('Leaflet');
+
+let myMap;
+
+// map options
+const options = {
+  // coordinates of UCSC from GPT
+  lat: 36.9914,
+  lng: -122.0609,
+  zoom: 15,    
+  pitch:0,
+  style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+}
+
+
+function setup(){
+  canvas = createCanvas(1200, 600); 
+
+  myMap = mappa.tileMap(options); 
+  myMap.overlay(canvas);
+}
+
+function draw(){
+    clear();
+  const itoshima = myMap.latLngToPixel(36.9914, -122.0609); 
+  ellipse(itoshima.x, itoshima.y, 10, 10);
 }
